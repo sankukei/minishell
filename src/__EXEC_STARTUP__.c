0f614b9673b664459eae198757b0bbde7816502a
Leo Thomas <leothoma@bess-f2r1s3.clusters.42paris.fr>
2025-05-07 14:05:10 +0200

    enfin

--- src/__EXEC_STARTUP__.c
+++ src/__EXEC_STARTUP__.c
@@ -83,43 +83,38 @@ char	**get_args(t_token **token)
 	return (res);	
 }
 
+void	close_all_pipes(int **pipes, int n)
+{
+	int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		close(pipes[i][0]);
+		close(pipes[i][1]);
+		i++;
+	}
+}
+
 int	__EXEC_STARTUP__(t_token *token)
 {
 	char	**args;
 	char		*cmd;
-
-
 	int	n;
 	pid_t	pid;
-	int	p[2];
 	int	status;
-	int	fd;
-	char	*line;
-
-	n = get_number_of_commands(token);
-	// CHECK IF HEREDOC IN TOKENS -> if so while readline until EOF and feed it to execve as is
 	int old_stdin = dup(STDIN_FILENO);
 	int old_stdout = dup(STDOUT_FILENO);
-	//old_stdin = dup(0);
-	//old_stdout = dup(1);
-//	if (n == 1)
-//	{
-//		cmd = token->str;
-//		if (!(args = get_args(&token)))
-//			return (0);	
-//		exec_single(cmd, args);
-//
-//	}
-	//array de pipe xd p[eut etyre 
 	int	**pipes;
-
-	pipes = malloc(n * sizeof(int *));
 	int	i = 0;
-	int	temp = n;
+	int	temp;
+
+	n = get_number_of_commands(token);
+	temp = n;
+	pipes = malloc(n * sizeof(int *));
 	temp -= 1;
 	while (temp--)
 	{
-		
 		printf("CREATE PIPE\n");
 		pipes[i] = malloc(sizeof(int) * 2);
 		pipe(pipes[i]);
@@ -133,22 +128,23 @@ int	__EXEC_STARTUP__(t_token *token)
 		args = get_args(&token);
 		if (pid == 0)
 		{
-			fprintf(stderr, "iam %s n = %d\n", args[0], i);
-			fflush(stderr);
+			//fprintf(stderr, "iam %s n = %d\n", args[0], i);
+			//fflush(stderr);
 			if (i != n - 1)
 			{
 				close(pipes[i][0]);
 				dup2(pipes[i][1], 1);
 				close(pipes[i][1]);
-				fprintf(stderr, "iam et j'ecris dans le pipe %s n = %d\n", args[0], i);
+				//fprintf(stderr, "iam et j'ecris dans le pipe %s n = %d\n", args[0], i);
 			}
 			if (i != 0)
 			{
-				fprintf(stderr, "iam et je lis dans le pipe %s n = %d\n", args[0], i);
+				//fprintf(stderr, "iam et je lis dans le pipe %s n = %d\n", args[0], i);
 				close(pipes[i - 1][1]);
 				dup2(pipes[i - 1][0], 0);
 				close(pipes[i -1][0]);
 			}
+			close_all_pipes(pipes, i);
 			//FONCTION QUI CLOSE TOUTE LRRAY DE PIPE
 			if (!exec_single(cmd, args))
 			{
@@ -172,40 +168,5 @@ int	__EXEC_STARTUP__(t_token *token)
 	dup2(old_stdout, STDOUT_FILENO);
 	close(old_stdout);
 	fprintf(stderr, "youhou je sors\n");
-	// int	number_of_forks;
-	// int	p[2];
-	// pid_t	pid;
-	// int	status;
-
-	// //number_of_commands = get_command_number(); //theoretical
-	// i = number_of_commands;
-	// while (number_of_forks--)
-	// {
-	// 	//dup stdin dans le pipe 0
-	// 	//dup stdout dans le pipe 0
-	// 	if (dup2(oldfd, newfd) == -1)
-	// 	{
-	// 		write(1, "error", 5);
-	// 		exit(1);
-	// 	}
-	// 	if (i == number_of_commands)
-	// 		dup2(stdin, p[2]);
-	// 	else
-	// 		dup2(p[2], stdout);
-	// 	pid = fork();
-	// 	if (pid < 0)
-	// 	{
-	// 		write(1, "no", 2);
-	// 		exit(1);
-	// 	}
-	// 	else if (pid == 0)
-	// 	{
-	// 		execve(token->str)
-	// 		//execve(data->token_;
-	// 	}
-	// }
-	// while (wait(&status) > 0)
-	// 	;
-	// return (0);
-
+	 return (0);
 }
